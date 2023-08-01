ad06ff107aab0c483d7727f07a85da85de906d40
UncleWeb1992 <watefalls@mail.ru>
2023-08-01 10:51:28 +0500

    add clear cart

--- src/pages/MessageList/MessageList.tsx
+++ src/pages/MessageList/MessageList.tsx
@@ -4,11 +4,13 @@ import { useAppDispatch, useAppSelector } from "../../hook/redux";
 import styles from "./MessageList.module.scss";
 import { formatDate } from "../../utils/date";
 import { CheckBox } from "../../components/common/CheckBox";
-import { MessageType } from "../../types/types";
+import { MessageFields, MessageType } from "../../types/types";
 import { useNavigate } from "react-router-dom";
 import { RoutePaths } from "../../config/routes/routes";
 import { deleteMessage, updateMessage } from "../../store/Slices/MailSlice";
 import { Relocate } from "../../components/ui/Relocate";
+
+import { FaTrash } from "react-icons/fa";
 
 const MessageList: FC = () => {
   const nav = useNavigate();
@@ -22,6 +24,8 @@ const MessageList: FC = () => {
   const currMsgs = subField
     ? messages.filter((msg) => msg.field === subField)
     : messages.filter((msg) => msg.type === parentField);
+
+  const cartMsgs = messages.filter((msg) => msg.type === MessageFields.Cart);
 
   const isChecked = (id: string) => !!selected.find((msg) => msg.id === id);
 
@@ -58,6 +62,16 @@ const MessageList: FC = () => {
 
   const deleteMsg = () => {
     selected.forEach((msg) => {
+      const find = messages.find((m) => m.id === msg.id);
+      if (find) {
+        selected.filter((msg) => msg.id !== find.id);
+        dispatch(updateMessage({ ...find, type: MessageFields.Cart }));
+      }
+    });
+  };
+
+  const clearCart = () => {
+    cartMsgs.forEach((msg) => {
       dispatch(deleteMessage({ id: msg.id }));
     });
   };
@@ -81,8 +95,12 @@ const MessageList: FC = () => {
         <div>
           {!!selected.length && (
             <ul className={styles.actions}>
-              <li onClick={markAsRead}>Отметить как прочитанные</li>
-              <li onClick={deleteMsg}>Удалить</li>
+              {parentField !== MessageFields.Cart && (
+                <li onClick={markAsRead}>Отметить как прочитанные</li>
+              )}
+              {parentField !== MessageFields.Cart && (
+                <li onClick={deleteMsg}>Удалить</li>
+              )}
               <li onClick={setNewField.bind(null, true)}>Переместить</li>
             </ul>
           )}
@@ -110,6 +128,13 @@ const MessageList: FC = () => {
             ))}
           </ul>
         </div>
+        {parentField === MessageFields.Cart && (
+          <FaTrash
+            onClick={clearCart}
+            title="Очистить корзину"
+            className={styles.clearCart}
+          />
+        )}
       </Layout>
       {newField && (
         <Relocate selectedMsgs={selected} onClose={closeModalField} />
